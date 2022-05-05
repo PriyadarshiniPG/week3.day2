ca146efa137bd9b871de71a09d2d71fa0c662135
PriyadarshiniPG <103844876+PriyadarshiniPG@users.noreply.github.com>
2022-05-05 12:12:21 +0530

    Add files via upload

---
+++ PrintDuplicatesInArray.java
@@ -0,0 +1,30 @@
+package week3.day2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+public class PrintDuplicatesInArray {
+	public static void main(String[] args) {
+		Integer[] array = { 14, 12, 13, 11, 15, 14, 18, 16, 17, 19, 18, 17, 20 };
+		List<Integer> list = new ArrayList<Integer>(Arrays.asList(array));
+
+		for (int i = 0; i < list.size(); i++) {
+			for (int j = i + 1; j < list.size(); j++) {
+				if (list.get(i).equals(list.get(j))) {
+
+					System.out.println(list.get(i));
+					break;
+
+				}
+
+			}
+		}
+
+	
+
+	}
+
+}
